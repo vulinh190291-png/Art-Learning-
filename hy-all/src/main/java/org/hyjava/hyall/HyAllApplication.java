25199dabe6b5c35272a898dd3c6b627ed030d457
IvanPC2 <1356986289@qq.com>
2025-11-13 17:30:43 +0800

    【开发】建立初步框架

---
+++ hy-all/src/main/java/org/hyjava/hyall/HyAllApplication.java
@@ -0,0 +1,13 @@
+package org.hyjava.hyall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyAllApplication.class, args);
+    }
+
+}
---
+++ hy-all/src/test/java/org/hyjava/hyall/HyAllApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hyjava.hyall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyAllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
