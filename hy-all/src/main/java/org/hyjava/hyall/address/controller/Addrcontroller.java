5c9374b6863aa12dd977330349c50758b4aa96e0
ATopos <3766309450@qq.com>
2025-11-15 17:35:52 +0800

    【开发】开发user和购物车

--- hy-all/src/main/java/org/hyjava/hyall/address/controller/Addrcontroller.java
+++ hy-all/src/main/java/org/hyjava/hyall/address/controller/Addrcontroller.java
@@ -1,5 +1,6 @@
 package org.hyjava.hyall.address.controller;
 
+import org.hyjava.hyall.address.service.IAddrservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- hy-app/unpackage/dist/cache/.vite/deps/axios.js
+++ hy-app/unpackage/dist/cache/.vite/deps/axios.js
@@ -2,14 +2,14 @@ import {
   __export
 } from "./chunk-P2LSHJDD.js";
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/helpers/bind.js
+// ../../../hy/hy-app/node_modules/axios/lib/helpers/bind.js
 function bind(fn, thisArg) {
   return function wrap() {
     return fn.apply(thisArg, arguments);
   };
 }
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/utils.js
+// ../../../hy/hy-app/node_modules/axios/lib/utils.js
 var { toString } = Object.prototype;
 var { getPrototypeOf } = Object;
 var { iterator, toStringTag } = Symbol;
@@ -380,7 +380,7 @@ var utils_default = {
   isIterable
 };
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/core/AxiosError.js
+// ../../../hy/hy-app/node_modules/axios/lib/core/AxiosError.js
 function AxiosError(message, code, config, request, response) {
   Error.call(this);
   if (Error.captureStackTrace) {
@@ -455,10 +455,10 @@ AxiosError.from = (error, code, config, request, response, customProps) => {
 };
 var AxiosError_default = AxiosError;
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/helpers/null.js
+// ../../../hy/hy-app/node_modules/axios/lib/helpers/null.js
 var null_default = null;
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/helpers/toFormData.js
+// ../../../hy/hy-app/node_modules/axios/lib/helpers/toFormData.js
 function isVisitable(thing) {
   return utils_default.isPlainObject(thing) || utils_default.isArray(thing);
 }
@@ -576,7 +576,7 @@ function toFormData(obj, formData, options) {
 }
 var toFormData_default = toFormData;
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/helpers/AxiosURLSearchParams.js
+// ../../../hy/hy-app/node_modules/axios/lib/helpers/AxiosURLSearchParams.js
 function encode(str) {
   const charMap = {
     "!": "%21",
@@ -609,7 +609,7 @@ prototype2.toString = function toString2(encoder) {
 };
 var AxiosURLSearchParams_default = AxiosURLSearchParams;
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/helpers/buildURL.js
+// ../../../hy/hy-app/node_modules/axios/lib/helpers/buildURL.js
 function encode2(val) {
   return encodeURIComponent(val).replace(/%3A/gi, ":").replace(/%24/g, "$").replace(/%2C/gi, ",").replace(/%20/g, "+").replace(/%5B/gi, "[").replace(/%5D/gi, "]");
 }
@@ -640,7 +640,7 @@ function buildURL(url, params, options) {
   return url;
 }
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/core/InterceptorManager.js
+// ../../../hy/hy-app/node_modules/axios/lib/core/InterceptorManager.js
 var InterceptorManager = class {
   constructor() {
     this.handlers = [];
@@ -704,23 +704,23 @@ var InterceptorManager = class {
 };
 var InterceptorManager_default = InterceptorManager;
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/defaults/transitional.js
+// ../../../hy/hy-app/node_modules/axios/lib/defaults/transitional.js
 var transitional_default = {
   silentJSONParsing: true,
   forcedJSONParsing: true,
   clarifyTimeoutError: false
 };
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/platform/browser/classes/URLSearchParams.js
+// ../../../hy/hy-app/node_modules/axios/lib/platform/browser/classes/URLSearchParams.js
 var URLSearchParams_default = typeof URLSearchParams !== "undefined" ? URLSearchParams : AxiosURLSearchParams_default;
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/platform/browser/classes/FormData.js
+// ../../../hy/hy-app/node_modules/axios/lib/platform/browser/classes/FormData.js
 var FormData_default = typeof FormData !== "undefined" ? FormData : null;
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/platform/browser/classes/Blob.js
+// ../../../hy/hy-app/node_modules/axios/lib/platform/browser/classes/Blob.js
 var Blob_default = typeof Blob !== "undefined" ? Blob : null;
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/platform/browser/index.js
+// ../../../hy/hy-app/node_modules/axios/lib/platform/browser/index.js
 var browser_default = {
   isBrowser: true,
   classes: {
@@ -731,7 +731,7 @@ var browser_default = {
   protocols: ["http", "https", "file", "blob", "url", "data"]
 };
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/platform/common/utils.js
+// ../../../hy/hy-app/node_modules/axios/lib/platform/common/utils.js
 var utils_exports = {};
 __export(utils_exports, {
   hasBrowserEnv: () => hasBrowserEnv,
@@ -749,13 +749,13 @@ var hasStandardBrowserWebWorkerEnv = (() => {
 })();
 var origin = hasBrowserEnv && window.location.href || "http://localhost";
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/platform/index.js
+// ../../../hy/hy-app/node_modules/axios/lib/platform/index.js
 var platform_default = {
   ...utils_exports,
   ...browser_default
 };
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/helpers/toURLEncodedForm.js
+// ../../../hy/hy-app/node_modules/axios/lib/helpers/toURLEncodedForm.js
 function toURLEncodedForm(data, options) {
   return toFormData_default(data, new platform_default.classes.URLSearchParams(), Object.assign({
     visitor: function(value, key, path, helpers) {
@@ -768,7 +768,7 @@ function toURLEncodedForm(data, options) {
   }, options));
 }
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/helpers/formDataToJSON.js
+// ../../../hy/hy-app/node_modules/axios/lib/helpers/formDataToJSON.js
 function parsePropPath(name) {
   return utils_default.matchAll(/\w+|\[(\w*)]/g, name).map((match) => {
     return match[0] === "[]" ? "" : match[1] || match[0];
@@ -822,7 +822,7 @@ function formDataToJSON(formData) {
 }
 var formDataToJSON_default = formDataToJSON;
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/defaults/index.js
+// ../../../hy/hy-app/node_modules/axios/lib/defaults/index.js
 function stringifySafely(rawValue, parser, encoder) {
   if (utils_default.isString(rawValue)) {
     try {
@@ -931,7 +931,7 @@ utils_default.forEach(["delete", "get", "head", "post", "put", "patch"], (method
 });
 var defaults_default = defaults;
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/helpers/parseHeaders.js
+// ../../../hy/hy-app/node_modules/axios/lib/helpers/parseHeaders.js
 var ignoreDuplicateOf = utils_default.toObjectSet([
   "age",
   "authorization",
@@ -976,7 +976,7 @@ var parseHeaders_default = (rawHeaders) => {
   return parsed;
 };
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/core/AxiosHeaders.js
+// ../../../hy/hy-app/node_modules/axios/lib/core/AxiosHeaders.js
 var $internals = Symbol("internals");
 function normalizeHeader(header) {
   return header && String(header).trim().toLowerCase();
@@ -1206,7 +1206,7 @@ utils_default.reduceDescriptors(AxiosHeaders.prototype, ({ value }, key) => {
 utils_default.freezeMethods(AxiosHeaders);
 var AxiosHeaders_default = AxiosHeaders;
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/core/transformData.js
+// ../../../hy/hy-app/node_modules/axios/lib/core/transformData.js
 function transformData(fns, response) {
   const config = this || defaults_default;
   const context = response || config;
@@ -1219,12 +1219,12 @@ function transformData(fns, response) {
   return data;
 }
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/cancel/isCancel.js
+// ../../../hy/hy-app/node_modules/axios/lib/cancel/isCancel.js
 function isCancel(value) {
   return !!(value && value.__CANCEL__);
 }
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/cancel/CanceledError.js
+// ../../../hy/hy-app/node_modules/axios/lib/cancel/CanceledError.js
 function CanceledError(message, config, request) {
   AxiosError_default.call(this, message == null ? "canceled" : message, AxiosError_default.ERR_CANCELED, config, request);
   this.name = "CanceledError";
@@ -1234,7 +1234,7 @@ utils_default.inherits(CanceledError, AxiosError_default, {
 });
 var CanceledError_default = CanceledError;
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/core/settle.js
+// ../../../hy/hy-app/node_modules/axios/lib/core/settle.js
 function settle(resolve, reject, response) {
   const validateStatus2 = response.config.validateStatus;
   if (!response.status || !validateStatus2 || validateStatus2(response.status)) {
@@ -1250,13 +1250,13 @@ function settle(resolve, reject, response) {
   }
 }
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/helpers/parseProtocol.js
+// ../../../hy/hy-app/node_modules/axios/lib/helpers/parseProtocol.js
 function parseProtocol(url) {
   const match = /^([-+\w]{1,25})(:?\/\/|:)/.exec(url);
   return match && match[1] || "";
 }
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/helpers/speedometer.js
+// ../../../hy/hy-app/node_modules/axios/lib/helpers/speedometer.js
 function speedometer(samplesCount, min) {
   samplesCount = samplesCount || 10;
   const bytes = new Array(samplesCount);
@@ -1292,7 +1292,7 @@ function speedometer(samplesCount, min) {
 }
 var speedometer_default = speedometer;
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/helpers/throttle.js
+// ../../../hy/hy-app/node_modules/axios/lib/helpers/throttle.js
 function throttle(fn, freq) {
   let timestamp = 0;
   let threshold = 1e3 / freq;
@@ -1327,7 +1327,7 @@ function throttle(fn, freq) {
 }
 var throttle_default = throttle;
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/helpers/progressEventReducer.js
+// ../../../hy/hy-app/node_modules/axios/lib/helpers/progressEventReducer.js
 var progressEventReducer = (listener, isDownloadStream, freq = 3) => {
   let bytesNotified = 0;
   const _speedometer = speedometer_default(50, 250);
@@ -1362,7 +1362,7 @@ var progressEventDecorator = (total, throttled) => {
 };
 var asyncDecorator = (fn) => (...args) => utils_default.asap(() => fn(...args));
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/helpers/isURLSameOrigin.js
+// ../../../hy/hy-app/node_modules/axios/lib/helpers/isURLSameOrigin.js
 var isURLSameOrigin_default = platform_default.hasStandardBrowserEnv ? /* @__PURE__ */ ((origin2, isMSIE) => (url) => {
   url = new URL(url, platform_default.origin);
   return origin2.protocol === url.protocol && origin2.host === url.host && (isMSIE || origin2.port === url.port);
@@ -1371,7 +1371,7 @@ var isURLSameOrigin_default = platform_default.hasStandardBrowserEnv ? /* @__PUR
   platform_default.navigator && /(msie|trident)/i.test(platform_default.navigator.userAgent)
 ) : () => true;
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/helpers/cookies.js
+// ../../../hy/hy-app/node_modules/axios/lib/helpers/cookies.js
 var cookies_default = platform_default.hasStandardBrowserEnv ? (
   // Standard browser envs support document.cookie
   {
@@ -1404,17 +1404,17 @@ var cookies_default = platform_default.hasStandardBrowserEnv ? (
   }
 );
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/helpers/isAbsoluteURL.js
+// ../../../hy/hy-app/node_modules/axios/lib/helpers/isAbsoluteURL.js
 function isAbsoluteURL(url) {
   return /^([a-z][a-z\d+\-.]*:)?\/\//i.test(url);
 }
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/helpers/combineURLs.js
+// ../../../hy/hy-app/node_modules/axios/lib/helpers/combineURLs.js
 function combineURLs(baseURL, relativeURL) {
   return relativeURL ? baseURL.replace(/\/?\/$/, "") + "/" + relativeURL.replace(/^\/+/, "") : baseURL;
 }
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/core/buildFullPath.js
+// ../../../hy/hy-app/node_modules/axios/lib/core/buildFullPath.js
 function buildFullPath(baseURL, requestedURL, allowAbsoluteUrls) {
   let isRelativeUrl = !isAbsoluteURL(requestedURL);
   if (baseURL && (isRelativeUrl || allowAbsoluteUrls == false)) {
@@ -1423,7 +1423,7 @@ function buildFullPath(baseURL, requestedURL, allowAbsoluteUrls) {
   return requestedURL;
 }
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/core/mergeConfig.js
+// ../../../hy/hy-app/node_modules/axios/lib/core/mergeConfig.js
 var headersToObject = (thing) => thing instanceof AxiosHeaders_default ? { ...thing } : thing;
 function mergeConfig(config1, config2) {
   config2 = config2 || {};
@@ -1503,7 +1503,7 @@ function mergeConfig(config1, config2) {
   return config;
 }
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/helpers/resolveConfig.js
+// ../../../hy/hy-app/node_modules/axios/lib/helpers/resolveConfig.js
 var resolveConfig_default = (config) => {
   const newConfig = mergeConfig({}, config);
   let { data, withXSRFToken, xsrfHeaderName, xsrfCookieName, headers, auth } = newConfig;
@@ -1536,7 +1536,7 @@ var resolveConfig_default = (config) => {
   return newConfig;
 };
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/adapters/xhr.js
+// ../../../hy/hy-app/node_modules/axios/lib/adapters/xhr.js
 var isXHRAdapterSupported = typeof XMLHttpRequest !== "undefined";
 var xhr_default = isXHRAdapterSupported && function(config) {
   return new Promise(function dispatchXhrRequest(resolve, reject) {
@@ -1663,7 +1663,7 @@ var xhr_default = isXHRAdapterSupported && function(config) {
   });
 };
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/helpers/composeSignals.js
+// ../../../hy/hy-app/node_modules/axios/lib/helpers/composeSignals.js
 var composeSignals = (signals, timeout) => {
   const { length } = signals = signals ? signals.filter(Boolean) : [];
   if (timeout || length) {
@@ -1699,7 +1699,7 @@ var composeSignals = (signals, timeout) => {
 };
 var composeSignals_default = composeSignals;
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/helpers/trackStream.js
+// ../../../hy/hy-app/node_modules/axios/lib/helpers/trackStream.js
 var streamChunk = function* (chunk, chunkSize) {
   let len = chunk.byteLength;
   if (!chunkSize || len < chunkSize) {
@@ -1776,7 +1776,7 @@ var trackStream = (stream, chunkSize, onProgress, onFinish) => {
   });
 };
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/adapters/fetch.js
+// ../../../hy/hy-app/node_modules/axios/lib/adapters/fetch.js
 var isFetchSupported = typeof fetch === "function" && typeof Request === "function" && typeof Response === "function";
 var isReadableStreamSupported = isFetchSupported && typeof ReadableStream === "function";
 var encodeText = isFetchSupported && (typeof TextEncoder === "function" ? /* @__PURE__ */ ((encoder) => (str) => encoder.encode(str))(new TextEncoder()) : async (str) => new Uint8Array(await new Response(str).arrayBuffer()));
@@ -1940,7 +1940,7 @@ var fetch_default = isFetchSupported && (async (config) => {
   }
 });
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/adapters/adapters.js
+// ../../../hy/hy-app/node_modules/axios/lib/adapters/adapters.js
 var knownAdapters = {
   http: null_default,
   xhr: xhr_default,
@@ -1994,7 +1994,7 @@ var adapters_default = {
   adapters: knownAdapters
 };
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/core/dispatchRequest.js
+// ../../../hy/hy-app/node_modules/axios/lib/core/dispatchRequest.js
 function throwIfCancellationRequested(config) {
   if (config.cancelToken) {
     config.cancelToken.throwIfRequested();
@@ -2039,10 +2039,10 @@ function dispatchRequest(config) {
   });
 }
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/env/data.js
+// ../../../hy/hy-app/node_modules/axios/lib/env/data.js
 var VERSION = "1.10.0";
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/helpers/validator.js
+// ../../../hy/hy-app/node_modules/axios/lib/helpers/validator.js
 var validators = {};
 ["object", "boolean", "number", "function", "string", "symbol"].forEach((type, i) => {
   validators[type] = function validator(thing) {
@@ -2106,7 +2106,7 @@ var validator_default = {
   validators
 };
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/core/Axios.js
+// ../../../hy/hy-app/node_modules/axios/lib/core/Axios.js
 var validators2 = validator_default.validators;
 var Axios = class {
   constructor(instanceConfig) {
@@ -2279,7 +2279,7 @@ utils_default.forEach(["post", "put", "patch"], function forEachMethodWithData(m
 });
 var Axios_default = Axios;
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/cancel/CancelToken.js
+// ../../../hy/hy-app/node_modules/axios/lib/cancel/CancelToken.js
 var CancelToken = class _CancelToken {
   constructor(executor) {
     if (typeof executor !== "function") {
@@ -2378,19 +2378,19 @@ var CancelToken = class _CancelToken {
 };
 var CancelToken_default = CancelToken;
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/helpers/spread.js
+// ../../../hy/hy-app/node_modules/axios/lib/helpers/spread.js
 function spread(callback) {
   return function wrap(arr) {
     return callback.apply(null, arr);
   };
 }
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/helpers/isAxiosError.js
+// ../../../hy/hy-app/node_modules/axios/lib/helpers/isAxiosError.js
 function isAxiosError(payload) {
   return utils_default.isObject(payload) && payload.isAxiosError === true;
 }
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/helpers/HttpStatusCode.js
+// ../../../hy/hy-app/node_modules/axios/lib/helpers/HttpStatusCode.js
 var HttpStatusCode = {
   Continue: 100,
   SwitchingProtocols: 101,
@@ -2461,7 +2461,7 @@ Object.entries(HttpStatusCode).forEach(([key, value]) => {
 });
 var HttpStatusCode_default = HttpStatusCode;
 
-// ../../../../Code/huiying_app2/node_modules/axios/lib/axios.js
+// ../../../hy/hy-app/node_modules/axios/lib/axios.js
 function createInstance(defaultConfig) {
   const context = new Axios_default(defaultConfig);
   const instance = bind(Axios_default.prototype.request, context);
@@ -2494,7 +2494,7 @@ axios.HttpStatusCode = HttpStatusCode_default;
 axios.default = axios;
 var axios_default = axios;
 
-// ../../../../Code/huiying_app2/node_modules/axios/index.js
+// ../../../hy/hy-app/node_modules/axios/index.js
 var {
   Axios: Axios2,
   AxiosError: AxiosError2,
