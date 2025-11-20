feceb3eb689ed1c4245015256eadf17a48ed76f1
IvanPC2 <1356986289@qq.com>
2025-11-20 10:31:40 +0800

    【开发】讲师

---
+++ hy-all/src/main/java/org/hyjava/hyall/instructor/repository/InstructorRepository.java
@@ -0,0 +1,7 @@
+package org.hyjava.hyall.instructor.repository;
+
+import org.hyjava.hyall.instructor.pojo.Instructor;
+import org.springframework.data.repository.CrudRepository;
+
+public interface InstructorRepository extends CrudRepository<Instructor,Integer> {
+}
